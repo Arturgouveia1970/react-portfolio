e033578b826c2367a4319da0e37e867e6f975a9d
Arturgouveia1970 <artgouveiag@gmail.com>
2022-11-07 15:04:23 +0100

    initial changes

---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Developer",
+          "Deep Learning Engineer",
+          "Full Stack Developer",
+          "Machine learning and algorithms",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
